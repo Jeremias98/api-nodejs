d6413f73e7907961b50f82446937e600498c897c
jeremias98 <ramirezjeremias98@gmail.com>
2019-09-14 13:13:12 -0300

    Primer Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+let usuario = {
+    nombre: '',
+    apellido: ''
+};
+
+app.get('/', function (req, res) {
+    res.send('Saludos desde express');
+});
+
+app.post('/hola', function (req, res) {
+    res.send('[POST]Saludos desde express');
+});
+
+app.get('/hola', function (req, res) {
+    res.send('[GET]Saludos desde express');
+});
+
+app.listen(3000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+});
